f247746c493acac3228d4327ace3ebd290496ce4
marcosrib <marcos.rib.sousa@gmail.com>
2019-04-07 21:49:28 -0300

    configuracao views path

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const dbConnection = sqlite.open(path.resolve(__dirname,'banco.sqlite'), Promise
 const bodyParser = require('body-parser')
 app.set('views',path.join(__dirname,'views'))
 app.set('view engine','ejs')
-app.use(express.static(path.join(__dirname,'public'))
+app.use(express.static(path.join(__dirname,'public')))
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', async (request, response)=> {
